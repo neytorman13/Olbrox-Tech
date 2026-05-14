c6e42fa94ee2db8f31e44e2944b89a3ef8cba488
neytorman13 <neytorman1998@hotmail.com>
2026-05-14 12:10:08 -0500

    Use middleware workaround for Vercel routing

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,7 @@
 import { updateSession } from '@/lib/db-middleware'
 import { type NextRequest } from 'next/server'
 
-export async function proxy(request: NextRequest) {
+export async function middleware(request: NextRequest) {
   return updateSession(request)
 }
 
